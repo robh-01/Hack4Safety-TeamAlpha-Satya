e17b010d186ebbdeec8b6070708fcf6f8184a060
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2026-06-13 09:15:35 +0545

    setup: add gitignore and init server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AI Detection System Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
